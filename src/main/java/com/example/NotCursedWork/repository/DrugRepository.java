6ec3998363ad07fd9f40c95c352d1db86ee7c667
Reinhard <deusexmachina1973@ya.ru>
2021-12-06 17:06:56 +0300

    DrugsDB added

---
+++ src/main/java/com/example/NotCursedWork/repository/DrugRepository.java
@@ -0,0 +1,7 @@
+package com.example.NotCursedWork.repository;
+
+import com.example.NotCursedWork.models.Drug;
+import org.springframework.data.repository.CrudRepository;
+
+public interface DrugRepository extends CrudRepository<Drug, Long> {
+}
